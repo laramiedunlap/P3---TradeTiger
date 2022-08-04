46644093fad0f15051cf32b91c5cfb152996960b
a-altman <altmanandrew03@gmail.com>
2022-08-03 22:48:31 -0400

    coded remaining functional elements of subscriber UI in subscriber_app.py. still need to work out a few bugs to write transaction data to csv and not print except message

--- Code/subscriber_app.py
+++ Code/subscriber_app.py
@@ -1,15 +1,17 @@
 from dataclasses import dataclass
 from typing import Any, List
 import datetime as datetime
+import pandas as pd
+import os
 from web3 import Web3
 from pathlib import Path
 from dotenv import load_dotenv
 import streamlit as st
-from Libraries.web3_contract import load_web3
+from Libraries.web3_contract import load_custom_web3
 
 load_dotenv()
 
-contract, address, w3 = load_web3("../Code/Contracts/ABI/Logger_abi.json")
+w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
 
 @dataclass
 class interface_block:
@@ -17,8 +19,7 @@ class interface_block:
     inputSubscriberAddress: str = ""
     inputEmail: str = ""
     inputSubCost: int = 0
-
-subscriptions = []
+    subscriptions = []
 
 @st.cache(allow_output_mutation=True)
 def setup():
@@ -29,27 +30,47 @@ accounts = w3.eth.accounts
 
 if st.sidebar.button("Manage TradeTiger Subscriptions"):
     with st.sidebar.container():
-        st.sidebar.write("click the dropdown to view your subscriptions", subscriptions)
+        st.sidebar.write("Click the dropdown to view your subscriptions", interface.subscriptions)
 
 st.title("Subscribe to a Trader")
 
-interface.inputEmail = st.text_input("enter email")
+interface.inputEmail = st.text_input("Enter email")
 interface.inputSubscriberAddress = st.selectbox("Choose Your Wallet Address for Payment", options=accounts)
-interface.inputTraderAddress = st.text_input("enter a trader's address")
-#sub_cost = 
-#interface.inputSubCost = st.
+interface.inputTraderAddress = st.text_input("Enter a trader's log address")
+if interface.inputTraderAddress != "":
+    try: 
+        contract, address, w3 = load_custom_web3("../Code/Contracts/ABI/sample_logger_contract_abi.json",interface.inputTraderAddress)
+        interface.inputSubCost = contract.functions.viewSubCost().call()
+        if st.button(f"Subscribe for {interface.inputSubCost} wei"):
+        
+            tx_hash = contract.functions.subscribe(
+            ).transact({'from': interface.inputSubscriberAddress, 'gas': 1000000, 'value': interface.inputSubCost})
+            receipt = dict(w3.eth.waitForTransactionReceipt(tx_hash))
+            st.write("Thanks for subscribing!")
+            st.write(receipt)
+            st.balloons()
+            sub_df = pd.read_csv('../Code/Libraries/sub.csv')
+            subscription_data = {
+                "traderAddress": interface.inputTraderAddress,
+                "subscriber_address": interface.inputSubscriberAddress,
+                "email_address": interface.inputEmail
+            }
+            sub_df.append(subscription_data)
+            sub_df.to_csv('../Code/Libraries/sub.csv')
+            subscriptions.append(receipt["inputTraderAddress"])
+    except:
+        st.write("Please try a different trader log address")
 
-if st.button(f"Send {interface.inputTraderAddress} subCost and Subscribe"):
-    tx_hash = w3.contract.functions.subscribe(
-        interface.inputTraderAddress,
-        interface.subCost
-    ).transact({'from': inputSubscriberAddress, 'gas': 1000000})
-    receipt = dict(w3.eth.waitForTransactionReceipt(tx_hash))
-    st.write("Thanks for subscribing!")
-    subscription_data = {
-        "traderAddress": inputTraderAddress,
-        "subscriberAddress": inputSubscriberAddress,
-        "subscriberEmail": inputEmail,
-        "paymentConfirmation": receipt["Hash"]
-    }
-    subscriptions.append(receipt["inputTraderAddress"])
+
+        # # open sub_df.csv, add row, save
+        # # csv takes [trader]
+        # sub_df = pd.read_csv('../Code/Libraries/sub.csv')
+        # subscription_data = {
+        #     "traderAddress": interface.inputTraderAddress,
+        #     "subscriber_address": interface.inputSubscriberAddress,
+        #     "email_address": interface.inputEmail
+        # }
+        # sub_df.append(subscription_data)
+        # sub_df.to_csv('../Code/Libraries/sub.csv')
+        
+        # subscriptions.append(receipt["inputTraderAddress"])
