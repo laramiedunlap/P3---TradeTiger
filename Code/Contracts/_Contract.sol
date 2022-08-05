c1a780ffbf1d052c219036f865733685f19e5add
JoeHGeoghegan <jhg55555@gmail.com>
2022-08-05 14:30:25 -0400

    Updating contract file

--- Code/Contracts/_Contract.sol
+++ Code/Contracts/_Contract.sol
@@ -3,16 +3,24 @@ contract logger {
     address payable trader;
     address payable server;
     uint256 TraderId;
+    uint subCost;
     uint tradeNum;
+    uint subNum;
+
     constructor(
         address payable newTrader,
         address payable _server,
-        uint _TraderId
+        uint _TraderId,
+        uint _subCost
     ) public { //Should check ERC20Detailed to be sure I am including all the security
         trader = newTrader;
         server = _server;
         TraderId = _TraderId;
+        subCost = _subCost;
+
         tradeNum = 0;
+        subNum = 0;
+
     }
     struct gradableTrade {
         uint TraderId ; 
@@ -27,6 +35,30 @@ contract logger {
         string optionsData;
         bool Verified ;
     }
+
+    event newSub (uint TraderId, uint subNum, address subAddr);
+
+    function subscribe () public payable{
+
+        require(msg.value == subCost);
+        
+        emit newSub(TraderId,subNum, msg.sender);
+
+        subNum ++ ;
+    }
+
+    function  viewSubCost () public view returns( uint) {
+        return (subCost);
+
+    }
+
+    function payout (address payable receiverAddr, uint amountWei) public payable  onlyServer {
+        
+        receiverAddr.transfer(amountWei);
+
+    }
+
+
     // gradableTrade blankTrade = gradableTrade(
     //     115792089237316195423570985008687907853269984665640564039457584007913129639935, //Nonce (solidity true max int)
     //     address(0), // traderAddress
@@ -42,6 +74,7 @@ contract logger {
     //     false, // isCall
     //     false //Verified
     // );
+
     modifier onlyTrader {
         require(msg.sender == trader, "You are not the trader associated with this log!");
         _;
@@ -123,12 +156,19 @@ contract logger {
     function checkServer () public view returns(address payable) {
         return (server);
     }
+
+    
+    // function subscribe (address payable) public {
+    
+
+    // }
 }
 contract deployer {
     address public deployer_address;
     address payable server;
     address payable newestTrader;
     uint TraderId ;
+    uint subCost ;
     constructor(
     ) public {
         server = msg.sender ;
@@ -136,10 +176,13 @@ contract deployer {
         TraderId = 0 ;
     }
     event logCreated(uint TraderId, address newLogAddress);
-    function createLog () public returns(address){
+
+    function createLog (uint _subCost) public returns(address){
+        subCost = _subCost;
         newestTrader = msg.sender;
         TraderId ++ ;
-        logger log = new logger(newestTrader, server, TraderId);
+        logger log = new logger(newestTrader, server, TraderId, subCost);
+
         emit logCreated(TraderId, address(log));
         return address(log);
     }
